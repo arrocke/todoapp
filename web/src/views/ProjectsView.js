3d2dd67bbe0bb78c69feb18e38cc8eb891bb2c15
Adrian Rocke <business@adrianrocke.com>
2019-04-07 16:57:10 -0500

    show paginated list of projects in UI

--- web/src/views/ProjectsView.js
+++ web/src/views/ProjectsView.js
@@ -1,21 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useGraphQL from '../hooks/graphql'
 
+const LIMIT = 5
+
 const ProjectsView = () => {
-  const [query, error] = useGraphQL({
-    query: `{
-      result: projects {
-        projects {
+  const [pageNumber, setPageNumber] = useState(0)
+  const [query] = useGraphQL({
+    query: `query Projects($input: ProjectsInput) {
+      projects(input: $input) {
+        page {
           name
           id
         }
+        hasNext
       }
-    }`
-  })
+    }`,
+    variables: {
+      input: {
+        pageNumber,
+        limit: LIMIT
+      }
+    }
+  }, [pageNumber])
 
-  return <div>
-    {query ? JSON.stringify(query.result.projects) : 'no projects'}
-  </div>
+  if (query) {
+    const projects = query.projects.page.map(p => <li key={p.id}>{p.name}</li>)
+
+    return <div>
+      <ul>
+        {projects}
+      </ul>
+      <p className="mt-4">
+        <button
+          className="btn"
+          disabled={pageNumber === 0}
+          type="button"
+          onClick={() => setPageNumber(pageNumber - 1)}
+        >Previous Page</button>
+        <span className="mx-2">{pageNumber + 1}</span>
+        <button
+          className="btn"
+          disabled={!query.projects.hasNext}
+          type="button"
+          onClick={() => setPageNumber(pageNumber + 1)}
+        >Next Page</button>
+      </p>
+    </div>
+  } else {
+    return null
+  }
 }
 
 export default ProjectsView
