756fe47109455e4ce9a7b601860fb39a1aaf07a0
Adrian Rocke <business@adrianrocke.com>
2019-04-30 18:04:51 -0500

    reset

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
-import Navigation from './components/Navigation'
 import PageView from './views/PageView'
 
 const App = () => {
   return (
     <Router>
-      <div className="h-full flex flex-col items-stretch bg-grey-light">
-        <Navigation className="flex-no-shrink"/>
-        <main className="flex-grow relative overflow-hidden p-4">
-          <PageView />
-        </main>
-      </div>
+      <PageView />
     </Router>
   );
 }
--- web/src/components/Card.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Card = ({ children, className }) => {
-  return <div className={`bg-white p-3 ${className}`}>
-    {children}
-  </div>
-}
-
-export default Card
