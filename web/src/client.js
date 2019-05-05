80523a95f8ba4834d2fef9ef05089ab354892281
Adrian Rocke <business@adrianrocke.com>
2019-05-04 22:45:28 -0500

    rewrite client to be mocked easier

--- web/src/client.js
+++ web/src/client.js
@@ -1,7 +1,7 @@
 import ApolloClient from "apollo-boost";
 import gql from "graphql-tag";
 
-const client = new ApolloClient({
+const apollo = new ApolloClient({
   uri: process.env.REACT_APP_GRAPHQL_URL,
 }, {
   defaultOptions: {
@@ -11,18 +11,19 @@ const client = new ApolloClient({
   }
 });
 
-// Rewrite query and mutate to auto compile graphql query strings.
-const queryRef = client.query.bind(client)
-const mutateRef = client.mutate.bind(client)
-
-client.query = ({ query, ...options }) => queryRef({
-  ...options,
-  query: gql`${query}`
-})
-
-client.mutate = ({ mutation, ...options }) => mutateRef({
-  ...options,
-  mutation: gql`${mutation}`
-})
+const client = {
+  query ({ query, ...options }) {
+    return apollo.query({
+      ...options,
+      query: gql`${query}`
+    })
+  },
+  mutate ({ mutation, ...options }) {
+    return apollo.mutate({
+      ...options,
+      mutation: gql`${mutation}`
+    })
+  }
+}
 
 export default client
