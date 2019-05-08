8e24e79de8d86cd8bbeb5c5a146d3fa34b2f6c81
Adrian Rocke <business@adrianrocke.com>
2019-05-08 00:43:57 -0500

    ignore act errors until react 16.9 is released

--- web/tests/unit/setup.js
+++ web/tests/unit/setup.js
@@ -1,10 +1,21 @@
 import styles from '../../src/styles.css'
 import { configure } from 'dom-testing-library'
 import 'jest-dom/extend-expect'
+import 'react-testing-library/cleanup-after-each'
 
 // Wait for styles to load.
 beforeAll(async () => {
+  // TODO: Remove when React 16.9 is released.
+  const error = console.error.bind(console)
+  jest.spyOn(console, 'error').mockImplementation((...args) => 
+    args[0].toString().includes('not wrapped in act(') || error(...args))
+
   await styles
+})
+
+afterAll(() => {
+  // TODO: Remove when React 16.9 is released.
+  console.error.mockRestore()
 })
 
 configure({
