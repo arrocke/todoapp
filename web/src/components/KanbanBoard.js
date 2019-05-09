0a8ffa17c35e7560fc88f8386097038b1966b64d
Adrian Rocke <business@adrianrocke.com>
2019-05-09 16:01:35 -0500

    refactor KanbanBoard

--- web/src/components/KanbanBoard.js
+++ web/src/components/KanbanBoard.js
@@ -1,47 +1,56 @@
 import React from 'react'
-import TaskList from './TaskList'
 
 const KanbanBoard = ({
   tasks = [],
   hideProject = false,
   className = ''
 } = {}) => {
-  const renderList = ({
+  const TaskCard = ({
+    task: { name, project } = {},
+    className = ''
+  } = {}) =>
+    <li className={className}>
+      <span data-test="task-name">{name}</span>
+      {
+        !hideProject && project
+          ? <span data-test="task-project">{project.name}</span>
+          : null
+      }
+    </li>
+
+  const KanbanList = ({
     title = '',
-    state: testState
+    state: testState,
+    className = ''
   }) =>
-    <div data-test="kanban-list">
+    <div 
+      className={className}
+      data-test="kanban-list"
+    >
       <h1 data-test="kanban-list-title">{title}</h1>
-      <TaskList
-        tasks={tasks.filter(({ state }) => state === testState)}
-        hideProject={hideProject}
-      />
+        <ul >
+          {
+            tasks
+              .filter(({ state }) => state === testState)
+              .map(
+                task => <TaskCard
+                  key={task.id}
+                  task={task}
+                />
+              )
+          } 
+        </ul>
     </div>
 
   return <div
     className={className}
     data-test="kanban-board"
   >
-    {renderList({
-      title: 'Added',
-      state: 'added'
-    })}
-    {renderList({
-      title: 'Planned',
-      state: 'planned'
-    })}
-    {renderList({
-      title: 'In Progress',
-      state: 'in-progress'
-    })}
-    {renderList({
-      title: 'Blocked',
-      state: 'blocked'
-    })}
-    {renderList({
-      title: 'Complete',
-      state: 'complete'
-    })}
+    <KanbanList title="Added" state="added" />
+    <KanbanList title="Planned" state="planned" />
+    <KanbanList title="In Progress" state="in-progress" />
+    <KanbanList title="Blocked" state="blocked" />
+    <KanbanList title="Complete" state="complete" />
   </div>
 }
 
