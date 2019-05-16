5579d72a5de532b30e64009bf64ad999c4671015
Adrian Rocke <business@adrianrocke.com>
2019-05-15 19:17:29 -0500

    refactor KanbanBoard component

--- web/src/components/kanban/KanbanBoard.js
+++ web/src/components/kanban/KanbanBoard.js
@@ -16,67 +16,52 @@ const KanbanBoard = ({
   const [visibleState, setVisibleState] = useState('in-progress')
   const [screen] = useMediaQuery()
 
-  const addButton = <button
-    className="fixed pin-r pin-b m-4 mb-14 lg:mb-4 rounded-full shadow bg-white w-12 h-12"
-    type="button" 
-    data-test="task-modal-button"
-    onClick={() => setTaskModalVisibility(true)}
-  />
-
-  const newTaskModal = <NewTaskModal show={showTaskModal} />
-
-  // Render all of the lists on larger screens.
-  if (screen.lg) {
-    // Generate a list for each task state.
-    const lists = STATES.map(state =>
+  const lgLists =
+    STATES.map(state =>
       <KanbanList
         key={state}
         state={state}
         hideProject={hideProject}
       />)
 
-    return <div
-      className={`flex-1 flex px-2 py-4 ${className}`}
-      data-test="kanban-board"
-    >
-      {addButton}
-      {lists}
-      {newTaskModal}
+  const smList =
+    <div className="flex-1 flex justify-center px-2 py-4">
+      <KanbanList
+        className="w-full max-w-sm"
+        state={visibleState}
+        hideProject={hideProject}
+      />
     </div>
-  }
 
-  // On smaller screens render a single list with buttons to select other lists to display. 
-  else {
-    // Generate a button for each task state.
-    const buttons = STATES.map(state =>
+  const navButtons = 
+    STATES.map(state =>
       <button
         key={state} state={state}
         className={`px-1 sm:px-2 py-3 text-2xs sm:text-xs md:text-sm font-bold ${ state === visibleState ? 'bg-grey-light' : ''} ${className}`}
         type="button"
         onClick={() => setVisibleState(state)}
-      >
-        {TITLE_MAP[state]}
-      </button>)
+      >{TITLE_MAP[state]}</button>)
 
-    return <div
-      className={`flex-1 flex flex-col max-h-full ${className}`}
-      data-test="kanban-board"
-    >
-      {addButton}
-      <div className="flex-1 flex justify-center px-2 py-4">
-        <KanbanList
-          className="w-full max-w-sm"
-          state={visibleState}
-          hideProject={hideProject}
-        />
-      </div>
-      <nav
+  const navigation =
+    <nav
         className="flex justify-center border-t-2 border-black"
         data-test="kanban-navigation"
-      >{buttons}</nav>
-      {newTaskModal}
-    </div>
-  }
+      >{navButtons}</nav>
+
+  return <div
+    className={`flex-1 flex flex-col lg:flex-row max-h-full lg:max-h-full lg:px-2 lg:py-4 ${className}`}
+    data-test="kanban-board"
+  >
+    <button
+      className="fixed pin-r pin-b m-4 mb-14 lg:mb-4 rounded-full shadow bg-white w-12 h-12"
+      type="button" 
+      data-test="task-modal-button"
+      onClick={() => setTaskModalVisibility(true)}
+    />
+    {screen.lg ? lgLists : smList}
+    {screen.lg ? null : navigation}
+    <NewTaskModal show={showTaskModal} />
+  </div>
 }
 
 export default KanbanBoard
