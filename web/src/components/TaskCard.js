67c69d09bf1ff2ee591ee9c2afb973f243f30154
Adrian Rocke <business@adrianrocke.com>
2019-05-16 21:34:47 -0500

    new task card

--- web/src/components/TaskCard.js
+++ web/src/components/TaskCard.js
@@ -1,67 +1,25 @@
-import React, { useState, useCallback } from 'react'
-import {STATES, TITLE_MAP} from '../config'
-import {useTasks} from '../contexts/task'
+import React from 'react'
 
-/**
- * Displays a task as a card. Includes UI to change the state of the task.
- * @param {Object} props.task The task to display.
- * @param {Boolean} props.hideProject Flag to hide the project name label for the task.
- */
 const TaskCard = ({
-  task,
-  hideProject = false,
-  className = ''
+  name,
+  project,
+  className,
+  ...props
 } = {}) => {
-  const {update} = useTasks()
-  const [menuOpen, setMenuState] = useState(false)
-
-  // Event handler to update the state of a task.
-  const onMenuClick = useCallback(state => {
-    update({ ...task, state })
-  }, [task])
-
-  // Event handler to begin dragging a task to another state list.
-  const onDragStart = useCallback(e => {
-    e.dataTransfer.setData('task', task)
-    e.dataTransfer.dropEffect = 'move'
-  }, [task])
-
-  // The list of buttons to change the task state.
-  // This does not include a button for the current task state.
-  const stateButtons = STATES
-    .filter(state => state !== task.state)
-    .map(state =>
-      <button
-        key={state}
-        type="button"
-        className="block py-2 font-bold text-sm text-right"
-        onClick={() => onMenuClick(state)}
-      >{TITLE_MAP[state]}</button>)
-
-  // The label for the tasks's project.
-  // Only rendered if projects are not hidden and the task has a project.
   const projectLabel = 
-    !hideProject && task.project
-      ? <span data-test="task-project">{task.project.name}</span>
+    project
+      ? <span data-test="task-project">{project}</span>
       : null
 
-  return <li
-    className={`my-2 p-2 rounded bg-white shadow cursor-pointer relative ${className}`}
+  return <div
+    className={`my-2 p-2 rounded bg-white shadow ${className}`}
     draggable="true"
-    onDragStart={onDragStart}
     data-test="task-card"
+    {...props}
   >
-    <span data-test="task-name">{task.name}</span>
+    <span data-test="task-name">{name}</span>
     {projectLabel}
-    <button
-      type="button"
-      className="float-right"
-      onClick={() => setMenuState(isOpen => !isOpen)}
-    >Move</button>
-    <div className={`absolute flex-col items-stretch pin-r mt-2 mr-2 px-3 py-1 bg-white z-10 shadow rounded-b ${menuOpen ? 'flex' : 'hidden'}`}>
-      {stateButtons}
-    </div>
-  </li>
+  </div>
 }
 
 export default TaskCard
