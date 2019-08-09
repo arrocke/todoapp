fc718b14de0d4a9891b1364f33eb0cfa9ac6b1f8
Adrian Rocke <business@adrianrocke.com>
2019-08-09 18:11:18 -0500

    display projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
+import { useProjects } from "./db-client";
 
 const App: React.FC = () => {
-  return <div></div>;
+  const { projects } = useProjects();
+
+  const projectElements = projects.map(project => (
+    <li key={project.id}>{project.name}</li>
+  ));
+  return <ul>{projectElements}</ul>;
 };
 
 export default App;
--- src/db-client.ts
+++ src/db-client.ts
@@ -1,6 +1,36 @@
 import Airtable from "airtable";
+import { useState, useEffect } from "react";
 
-export default new Airtable({
+const base = new Airtable({
   endpointUrl: "https://api.airtable.com",
   apiKey: process.env.REACT_APP_AIRTABLE_API_KEY
 }).base(process.env.REACT_APP_AIRTABLE_BASE);
+
+export interface ProjectRecord extends Airtable.FieldSet {
+  id: string;
+  name: string;
+}
+
+export interface TaskRecord extends Airtable.FieldSet {
+  id: string;
+  name: string;
+}
+
+const db = {
+  projects: base("Projects") as Airtable.Table<ProjectRecord>,
+  tasks: base("Tasks") as Airtable.Table<TaskRecord>
+};
+
+export function useProjects() {
+  const [projects, setProjects] = useState<ProjectRecord[]>([]);
+
+  useEffect(() => {
+    async function effect() {
+      const records = await db.projects.select().all();
+      setProjects(records.map(({ fields, id }) => ({ ...fields, id })));
+    }
+    effect();
+  }, []);
+
+  return { projects };
+}
