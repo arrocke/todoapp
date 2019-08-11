5ceddb509dbb021752a1c7e1513b686b47e1be62
Adrian Rocke <business@adrianrocke.com>
2019-08-10 20:16:46 -0500

    basic navigation

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import ProjectsView from "./ProjectsView";
 import ProjectView from "./ProjectView";
 import TasksView from "./TasksView";
+import Navigation from "./Navigation";
 
 const App: React.FC = () => {
   return (
     <Router>
+      <Navigation />
       <Route exact path="/projects" component={ProjectsView} />
       <Route exact path="/projects/:id" component={ProjectView} />
       <Route exact path="/tasks" component={TasksView} />
---
+++ src/Navigation.tsx
@@ -0,0 +1,26 @@
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import { Link } from "react-router-dom";
+
+const linkStyles = css({
+  margin: 8
+});
+
+const Navigation: React.FC = () => {
+  return (
+    <nav
+      css={{
+        display: "flex"
+      }}
+    >
+      <Link css={linkStyles} to="/projects">
+        Projects
+      </Link>
+      <Link css={linkStyles} to="/tasks">
+        Tasks
+      </Link>
+    </nav>
+  );
+};
+
+export default Navigation;
