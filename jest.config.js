1e75705784e047fe747f859330a9e157e55d1ab8
Adrian Rocke <me@adrianrocke.com>
2019-08-16 22:47:46 -0500

    reorganize into monorepo

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  watchPlugins: [
-    "jest-watch-typeahead/filename",
-    "jest-watch-typeahead/testname"
-  ],
-  collectCoverageFrom: ["**/src/**/*!(.{test,spec}).{js,jsx,ts,tsx}"],
-  coveragePathIgnorePatterns: ["/node_modules/"]
-};
