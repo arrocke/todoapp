52d0b4feb372cf62f2e10b3453595e00ef2b69a2
Adrian Rocke <me@adrianrocke.com>
2019-09-17 23:08:14 -0500

    beter loading spinner

--- web/src/components/LoadingContainer.tsx
+++ web/src/components/LoadingContainer.tsx
@@ -1,18 +1,71 @@
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
+import { jsx, keyframes } from "@emotion/core";
+import { readerOnly } from "../styles";
+import { useState, useEffect } from "react";
 
 interface LoadingContainerProps {
   className?: string;
   isLoading: boolean;
 }
 
+const spinner = keyframes({
+  "0%": {
+    transform: "rotate(0deg)"
+  },
+  "100%": {
+    transform: "rotate(360deg)"
+  }
+});
+
 const LoadingContainer: React.FC<LoadingContainerProps> = ({
   className,
   isLoading,
   children
 }) => {
-  const loader = <div>Loading...</div>;
-  return <div className={className}>{isLoading ? loader : children}</div>;
+  const [stall, setStall] = useState<boolean>(true);
+
+  useEffect(() => {
+    setStall(false);
+  }, []);
+
+  const loader = (
+    <div
+      css={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <div
+        css={{
+          display: "inline-block",
+          width: 64,
+          height: 64,
+          "::after": {
+            content: "' '",
+            display: "block",
+            width: 46,
+            height: 46,
+            margin: 1,
+            borderRadius: "50%",
+            border: "5px solid black",
+            borderColor: "black transparent black transparent",
+            animation: `${spinner} 1.2s linear infinite`
+          }
+        }}
+      ></div>
+    </div>
+  );
+  return (
+    <div className={className}>
+      <div css={readerOnly} role="alert" aria-live="assertive">
+        {!stall && <p>{isLoading ? "Content Loading" : "Content Loaded"}</p>}
+      </div>
+      {isLoading ? loader : children}
+    </div>
+  );
 };
 
 export default LoadingContainer;
--- web/src/styles.ts
+++ web/src/styles.ts
@@ -1,3 +1,16 @@
+import { Interpolation } from "@emotion/css";
+
 export const breakpoints = {
   medium: "@media (min-width: 768px)"
+};
+
+export const readerOnly: Interpolation = {
+  border: 0,
+  clip: "rect(0 0 0 0)",
+  height: 1,
+  margin: -1,
+  overflow: "hidden",
+  padding: 0,
+  position: "absolute",
+  width: 1
 };
