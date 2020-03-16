420636fd9f9e4600f42e4114fb6eaf0baec975d4
Adrian Rocke <me@adrianrocke.com>
2020-03-16 16:36:17 -0500

    reset web project

--- web/pages/about.tsx
+++
@@ -1,5 +0,0 @@
-const Index: React.FC = () => {
-  return <div>Hello next.js</div>
-}
-
-export default Index
--- web/styles/icons.ts
+++ web/styles/icons.ts
@@ -1,9 +1,4 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faBars,
-  faUser,
-  faMicroscope,
-  faTimes
-} from "@fortawesome/free-solid-svg-icons";
+import {} from "@fortawesome/free-solid-svg-icons";
 
-library.add(faBars, faUser, faMicroscope, faTimes);
+library.add();
