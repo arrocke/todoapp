e87dc49fbb56ccfb84e34955f50874ade2bd8239
Adrian Rocke <me@adrianrocke.com>
2020-03-21 13:33:53 -0500

    document Result and ValueObject

--- api/src/core/Result.ts
+++ api/src/core/Result.ts
@@ -1,8 +1,17 @@
+/** Returns a successful or failed result from an operation. */
 export default class Result<T> {
-  public readonly isSuccess: boolean;
-  public readonly isFailure: boolean;
   private _value?: T | Error;
 
+  /** True if result is a success. */
+  public readonly isSuccess: boolean;
+
+  /** True if result is a failure. */
+  public readonly isFailure: boolean;
+
+  /**
+   * The error if result is a failure.
+   * @throws Throws {Error} result is not a failure.
+   */
   get error(): Error {
     if (this.isSuccess) {
       throw new Error(
@@ -12,6 +21,10 @@ export default class Result<T> {
     return this._value as Error;
   }
 
+  /**
+   * The value if result is a success.
+   * @throws Throws {Error} result is not a success.
+   */
   get value(): T {
     if (this.isFailure) {
       throw new Error(
@@ -21,7 +34,7 @@ export default class Result<T> {
     return this._value as T;
   }
 
-  public constructor(isSuccess: boolean, value?: T | Error) {
+  private constructor(isSuccess: boolean, value?: T | Error) {
     if (isSuccess && value instanceof Error) {
       throw new Error(
         "InvalidOperation: A success result cannot contain an error"
@@ -40,10 +53,18 @@ export default class Result<T> {
     Object.freeze(this);
   }
 
+  /**
+   * Creates a successful result with an optional value.
+   * @param value The result value.
+   */
   public static ok<U>(value?: U): Result<U> {
     return new Result<U>(true, value);
   }
 
+  /**
+   * Creates a failed result with an optional error.
+   * @param error The error object or message.
+   */
   public static fail<U>(error?: Error | string): Result<U> {
     return new Result<U>(
       false,
@@ -51,6 +72,12 @@ export default class Result<T> {
     );
   }
 
+  /**
+   * Creates a single result object from a list of results
+   * that is successful if all results are successful,
+   * or is a failure if one is a failure.
+   * @param results The list of results to combine.
+   */
   public static combine(...results: Result<any>[]): Result<void> {
     for (const result of results) {
       if (result.isFailure) return Result.fail(result.error);
--- api/src/core/ValueObject.ts
+++ api/src/core/ValueObject.ts
@@ -1,12 +1,27 @@
 import shallowEqual from "utils/shallow-equal";
 
+/** Base class for creating value objects. */
 export default abstract class ValueObject<T extends {}> {
+  /**
+   * The data of the ValueObject.
+   * This is reserved for use by mappers
+   * and should not be used within the domain.
+   */
   public readonly props: T;
 
+  /**
+   * Creates a new ValueObject with data and prevents it from being modified.
+   * This should be called from a derived classes static create method.
+   * @param props The data to create the ValueObject with.
+   */
   protected constructor(props: T) {
     this.props = Object.freeze(props);
   }
 
+  /**
+   * Compares another ValueObject by their value.
+   * @param vo The other ValueObject to compare.
+   */
   public equals(vo?: ValueObject<T>): boolean {
     return (
       vo !== null &&
